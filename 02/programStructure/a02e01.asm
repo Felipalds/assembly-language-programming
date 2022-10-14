0ed5c4ce43008ecb5e2986f2e8cdb2c5adbaa779
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2022-10-14 11:09:45 -0300

    data manipulating

--- 02/programStructure/a02e01.asm
+++ 02/programStructure/a02e01.asm
@@ -4,7 +4,7 @@
 ; nasm -f elf64 a02e01.asm ; ld a02e01.o -o a02e01.x
 
 section .data
-    v1:  db    0x0CED              ; byte 0x55 
+    v1:  db    0xED              ; byte 0x55 
     v2:  db    0x55,0x56,0x57    ; 3 bytes em sucessao 
     v3:  db    'a',0x55          ; caracteres sao aceitos com aspas 
     v4:  db    'hello',13,10,0   ; strings tambem 
@@ -12,8 +12,8 @@ section .data
     v6:  dw    'a'               ; 0x61 0x00 
     v7:  dw    'ab'              ; 0x61 0x62
     v8:  dw    'abc'             ; 0x61 0x62 0x63 0x00 (string) 
-    v9:  dd   4295123123     ; 0x78 0x56 0x34 0x12
-    v10: dq 
+    v9:  dd   1233     ; 0x78 0x56 0x34 0x12
+    v10: dw   'luiz'   
 
 section .text
     global _start
